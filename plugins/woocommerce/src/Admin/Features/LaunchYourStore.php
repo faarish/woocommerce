f4888a0e34679da391930442417bb2d78e4a02d6
moon <moon.kyong@automattic.com>
2024-04-04 14:17:45 -0700

    Use user meta to save woocommerce_coming_soon_banner_dismissed

--- plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
+++ plugins/woocommerce/src/Admin/Features/LaunchYourStore.php
@@ -109,7 +109,13 @@ class LaunchYourStore {
 	 * - 'woocommerce_coming_soon' option value must be 'yes'
 	 */
 	public function maybe_add_coming_soon_banner_on_frontend() {
-		if ( get_option( 'woocommerce_coming_soon_banner_dismissed' ) === 'yes' ) {
+
+		$current_user_id = get_current_user_id();
+		if ( ! $current_user_id ) {
+			return false;
+		}
+
+		if ( get_user_meta( $current_user_id, 'woocommerce_coming_soon_banner_dismissed', true ) === 'yes' ) {
 			return false;
 		}
 		// User must be an admin or editor.
